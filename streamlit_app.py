fc89bf15c1fe88558b9991e3a300c1f279d8d65c
lars-uhv <lars.thomassen@chimaera.dk>
2024-07-20 14:03:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ st.write('The name on your Smoothie will be:',name_on_order)
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe,use_container_width=True)
-#st.stop()
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 # Convert the Snowpark Dataframe to a  Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
