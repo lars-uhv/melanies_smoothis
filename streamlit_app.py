1d433ab58c749a9121b8c9b76d8303cdf1c96acf
lars-uhv <lars.thomassen@chimaera.dk>
2024-07-20 14:11:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ pd_df = my_dataframe.to_pandas()
 #st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
-    ,my_dataframe
+    ,my_dataframe.select(col('FRUIT_NAME'))
     ,max_selections = 5
 )
 if ingredients_list:
